1211d7c208d4a2d3346bd4b4ed16bd7c88efb567
Tal Ben-Ari <talbenari1@gmail.com>
2014-06-18 23:45:26 -0700

    Basic HTTP REST server w/ Rethinkdb started.

---
+++ server.js
@@ -0,0 +1,46 @@
+var thinky = require('thinky')({db: 'password_manager'}),
+    app = require('express')(),
+    bodyParser = require('body-parser'),
+    r = thinky.r;
+
+app.use(bodyParser.json());
+
+var User = thinky.createModel('User', {
+    id: String,
+    firstName: String,
+    lastName: String
+});
+
+User.ensureIndex('firstName');
+
+app.route('/').get(function (req, res) {
+    res.json({main: 'It works!'});
+});
+
+app.route('/users')
+    .get(function (req, res) {
+        User.orderBy({index: 'firstName'}).run().then(function (users) {
+            res.json(users)
+        });
+    })
+    .post(function (req, res) {
+        var user = new User(req.body);
+        user.save().then(function (user) {
+            console.log('User %s %s added to the database.', user.firstName, user.lastName);
+            res.status(201);
+            res.send(user.id);
+        });
+    });
+
+app.route('/users/:id')
+    .delete(function (req, res) {
+        User.get(req.params.id).run().then(function (user) {
+            user.delete().then(function (user) {
+                console.log('User %s %s removed from the database.', user.firstName, user.lastName);
+                res.status(204);
+                res.send();
+            })
+        });
+    });
+
+app.listen(process.env.PORT || 3000);
