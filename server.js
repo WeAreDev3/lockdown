7842336413af2ea2c3b7336fea511f88dffc5b84
Jesse Smick <janka102@gmail.com>
2014-06-22 15:55:45 -0700

    Improved how to search for duplicate usernames

--- server.js
+++ server.js
@@ -27,85 +27,81 @@ app.route('/').get(function(req, res) {
 });
 
 app.route('/signin')
-    // Sign in the user
-    .post(function(req, res, next) {
-        passport.authenticate('local', function(err, user, info) {
+// Sign in the user
+.post(function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            // 500 (Internal Server Error)
+            return res.send(500, err);
+        }
+
+        if (!user) {
+            // Credentials not correct, send 401 (Unauthorized)
+            return res.send(401, info);
+        }
+
+        req.logIn(user, function(err) {
             if (err) {
                 // 500 (Internal Server Error)
                 return res.send(500, err);
             }
 
-            if (!user) {
-                // Credentials not correct, send 401 (Unauthorized)
-                return res.send(401, info);
-            }
-
-            req.logIn(user, function(err) {
-                if (err) {
-                    // 500 (Internal Server Error)
-                    return res.send(500, err);
-                }
-
-                res.send(200);
-            });
-        })(req, res, next);
-    });
+            res.send(200);
+        });
+    })(req, res, next);
+});
 
 
 app.route('/session')
-    // Delete the users's session, i.e. sign them out
-    .delete(function(req, res) {
-        req.logout();
-        res.send(204);
-    });
+// Delete the users's session, i.e. sign them out
+.delete(function(req, res) {
+    req.logout();
+    res.send(204);
+});
 
 app.route('/users')
-    // Get a list of all the users
-    .get(auth.ensureAuthenticated, function(req, res) {
-        User.orderBy({
-            index: 'username'
-        }).run().then(function(users) {
-            res.json(users);
+// Get a list of all the users
+.get(auth.ensureAuthenticated, function(req, res) {
+    User.orderBy({
+        index: 'username'
+    }).run().then(function(users) {
+        res.json(users);
+    }, function(err) {
+        res.json(err);
+    });
+})
+// Create a new user
+.post(function(req, res) {
+    User.getAll(req.body.username, {
+        index: 'username'
+    }).run().then(function(dupeUser) {
+        if (dupeUser.length) {
+            console.log('Username "%s" already exits, %s %s', req.body.userame, req.body.firstName, req.body.lastName);
+
+            // Username already exits, send 409 (Conflict)
+            res.status(409);
+            return res.send({
+                message: 'A user with that username already exists'
+            });
+        }
+
+        user = new User(req.body);
+        user.save().then(function(user) {
+            console.log('User %s %s added to the database.', user.firstName, user.lastName);
+
+            res.status(201);
+            res.send(user.id);
         }, function(err) {
-            res.json(err);
-        });
-    })
-    // Create a new user
-    .post(function(req, res) {
-        User.run().then(function(users) {
-            var i = users.length,
-                user;
+            console.log('User %s %s NOT created', user.firstName, user.lastName);
+            console.log(err.toString());
 
-            // Check if any user already has that username
-            while (i--) {
-                if (req.body.username === users[i].username) {
-                    console.log('Username "%s" already exits, %s %s', req.body.userame, req.body.firstName, req.body.lastName);
-
-                    // Username already exits, send 409 (Conflict)
-                    res.status(409);
-                    return res.send({
-                        message: 'A user with that username already exists'
-                    });
-                }
-            }
-
-            user = new User(req.body);
-            user.save().then(function(user) {
-                console.log('User %s %s added to the database.', user.firstName, user.lastName);
-
-                res.status(201);
-                res.send(user.id);
-            }, function(err) {
-                console.log('User %s %s NOT created', user.firstName, user.lastName);
-                console.log(err.toString());
-
-                res.status(400);
-                res.send({
-                    message: err.toString()
-                });
+            res.status(400);
+            res.send({
+                message: err.toString()
             });
         });
     });
+});
 
 app.route('/users/:id')
     .delete(auth.ensureAuthenticated, function(req, res) {
