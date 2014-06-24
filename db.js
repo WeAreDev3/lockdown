ac2a22ad947bfb7a574ec7ab44d9f89924c2e739
mkeedlinger <mkeedlinger@gmail.com>
2014-06-23 21:11:32 -0700

    restructured db for better exporting, also added more tables for later uses

--- db.js
+++ db.js
@@ -3,36 +3,52 @@ var thinky = require('thinky')({
     }),
     r = thinky.r;
 
-
-var User = thinky.createModel('Users', {
-    id: String,
-    firstName: String,
-    lastName: String,
-    username: String,
-    passHash: String,
-    passSalt: String,
-    passIter: Number,
-    passHashSize: Number,
-    timestamp: {
-        _type: Date,
-        default: r.now(),
-        options: {
-            enforce_extra: false
+def = {
+    User: thinky.createModel('Users', {
+        id: String,
+        firstName: String,
+        lastName: String,
+        username: String,
+        passHash: String,
+        passSalt: String,
+        passIter: Number,
+        passHashSize: Number,
+        timestamp: {
+            _type: Date,
+            default: r.now(),
+            options: {
+                enforce_extra: false
+            }
+        },
+        displayUsername: String,
+        sites: Array,
+        clientCrypt: {
+            hashSize: Number,
+            iter: Number,
+            salt: String
         }
-    },
-    displayUsername: String,
-    sites: Array,
-    clientCrypt: {
-        hashSize: Number,
-        iter: Number,
-        salt: String
-    }
-}, {
-    enforce_type: 'strict', // Do not allow null to be a valid value
-    enforce_extra: true // do not allow extra data
-});
+    }, {
+        enforce_type: 'strict', // Do not allow null to be a valid value
+        enforce_extra: true // do not allow extra data
+    }),
 
-User.ensureIndex('username');
+    Stats: thinky.createModel('Stats', {
+        id: String
+    }),
 
-module.exports.User = User;
-module.exports.r = thinky.r;
+    Config: thinky.createModel('Config', {
+        id: String,
+        timestamp: {
+            _type: Date,
+            default: r.now(),
+            options: {
+                enforce_extra: false
+            }
+        },
+        sessonSecret: String
+    }),
+    
+    r: thinky.r
+}
+
+def.User.ensureIndex('username');
